9c3118d60a1cd6cd087f70acf09e162cf3cbee30
alexpetre27 <147337105+alexpetre27@users.noreply.github.com>
2026-05-16 22:42:21 +0300

    lc122

---
+++ lc122/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int result = solution.maxProfit(prices);
+        System.out.println("Maximum profit: " + result);
+    }
+}
