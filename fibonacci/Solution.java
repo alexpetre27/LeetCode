16ebe9d5d1d0adb66b520cb0f2712fdcd2d99bf4
Paul <petrealexandru1152@gmail.com>
2026-06-13 14:09:38 +0300

    fibonacci

---
+++ fibonacci/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.fibonacci(10));
+    }
+}
